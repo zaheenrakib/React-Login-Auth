9d31e3a167172fca8d5fd1681875a4f4a84eef17
zaheenrakib <rakibislambest@gmail.com>
2024-04-01 10:42:40 +0600

    Setup Projects

---
+++ src/__auth__names.js
@@ -0,0 +1,5 @@
+/**
+ * 1.Authentication , authorization
+ * 2.login === sign in
+ * 3.register == sign up
+ */
---
+++ src/main.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home/Home.jsx';
+import Login from './components/Login/Login.jsx';
+import Register from './components/Register/Register.jsx';
+import Root from './components/Root/Root.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/login",
+        element: <Login></Login>
+      },
+      {
+        path: "/register",
+        element: <Register></Register>
+      }
+    ]
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
