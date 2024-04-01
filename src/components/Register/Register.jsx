e7f071ed38516b5ec7bbe188a67f602d0ea19747
zaheenrakib <rakibislambest@gmail.com>
2024-04-01 15:21:17 +0600

    Validation

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,22 +1,40 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-import React from 'react';
+import React, { useState } from 'react';
 import auth from '../../firebase/firebase.config';
 
 const Register = () => {
 
+    const [registerError , setRegisterError] = useState('');
+    const [success , setSuccess] = useState('')
 
     const handleRegister = e => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
         console.log(email,password);
+
+        //reset error
+        setRegisterError('');
+        setSuccess('');
+
+        
+
+        if(password.length , 6){
+            setRegisterError('Password Should be at least 6 characters or longer');
+            return;
+        }
+
+        
+
         // create user
         createUserWithEmailAndPassword(auth,email,password)
         .then(result =>{
             console.log(result.user);
+            setSuccess("User Create Successfully.");
         })
         .catch(error => {
              console.log(error);
+             setRegisterError(error.message)
         })
 
     }
@@ -26,12 +44,18 @@ const Register = () => {
             <div className='mx-auto md:w-1/2'>
                 <h2 className='text-3xl mb-8'> Please Register </h2>
                 <form onSubmit={handleRegister}>
-                    <input className='mb-4 w-3/4 py-2 px-4' type="email" name='email' placeholder='Email Address' id='' />
+                    <input className='mb-4 w-3/4 py-2 px-4' type="email" name='email' placeholder='Email Address' required />
                     <br />
-                    <input className='mb-4 w-3/4 py-2 px-4' type="password" name='password' placeholder='Password' id='' />
+                    <input className='mb-4 w-3/4 py-2 px-4' type="password" name='password' placeholder='Password' required />
                     <br />
                     <input className='mb-4 w-3/4 btn btn-secondary' type="submit" value='Register' />
                 </form>
+                {
+                    registerError && <p className='text-red-500'>{registerError}</p>
+                }
+                {
+                    success && <p className='text-green-500'>{success}</p>
+                }
             </div>
         </div>
     );
